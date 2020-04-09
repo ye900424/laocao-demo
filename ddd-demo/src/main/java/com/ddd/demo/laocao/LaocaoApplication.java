dda8c3f0d8cb22f28c2da73a5d25628fffaf0bbe
老曹 <caoyang@cai-inc.com>
2020-04-09 14:11:19 +0800

    [添加两个模块]

---
+++ ddd-demo/src/main/java/com/ddd/demo/laocao/LaocaoApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.demo.laocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaocaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaocaoApplication.class, args);
+    }
+
+}
---
+++ ddd-demo/src/test/java/com/ddd/demo/laocao/LaocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.demo.laocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaocaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
